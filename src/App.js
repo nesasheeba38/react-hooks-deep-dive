97232b28ced2382ec38ee8ace84cdd6e4d008970
nesasheeba38 <nesasheebaj32>
2026-02-01 13:26:42 +0530

    seat availability dashboard using useMemo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import SeatAvailabilityDashboard from './Components/SeatAvailabilityDashboardByUseMemo';
+import SeatAvailabilityDashboardByUseMemo from './Components/SeatAvailabilityDashboardByUseMemo';
 
 function App() {
   return (
   <div>
-  <SeatAvailabilityDashboard/>
+  <SeatAvailabilityDashboardByUseMemo/>
   </div>
   );
 }
--- src/Components/SeatAvailabilityDashboardByUseMemo.jsx
+++ src/Components/SeatAvailabilityDashboardByUseMemo.jsx
@@ -3,20 +3,22 @@ import { useMemo, useState } from "react";
 const totalSeats = {
   Silver: 30,
   Gold: 20,
-  Diamond: 10,
+  Diamond: 10
 };
+
 
 const bookedSeats = {
   Silver: [2, 5, 8, 15],
   Gold: [1, 4, 7],
-  Diamond: [3, 6],
+  Diamond: [3, 6]
 };
 
-const SeatAvailabilityDashboard = () => {
+const SeatAvailabilityDashboardByUseMemo = () => {
   const [section, setSection] = useState("");
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState("light"); 
 
-  const availableSeats = useMemo(() => {
+    
+    const availableSeats =useMemo(() => {
     console.log("Calculating available seats...");
 
     const seats = [];
@@ -30,45 +32,39 @@ const SeatAvailabilityDashboard = () => {
     }
 
     return seats;
-  }, [section]);
+  },[section]);  
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <h2>🎟️ Event Seat Booking</h2>
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center"
+      }} >
+        
+      <h2>🎟️ Movie  Seat Booking</h2>
 
       <div>
-        <label>Select Section: </label>
-        <select value={section} onChange={(e) => setSection(e.target.value)}>
-          <option>Select Seat Type</option>
-          <option value="Silver">Silver</option>
-          <option value="Gold">Gold</option>
-          <option value="Diamond">Diamond</option>
-        </select>
+      <label>Select Section: </label>
+      <select value={section}onChange={(e) => setSection(e.target.value)}>
+        <option>Select Seat Type</option>
+        <option value="Silver">Silver</option>
+        <option value="Gold">Gold</option>
+        <option value="Diamond">Diamond</option>
+      </select>
       </div>
-      <br />
-      <br />
+      <br /><br />
 
-      <button onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
-        Toggle Theme
-      </button>
+      <button onClick={() => setTheme(theme === "light" ? "dark" : "light")}>Toggle Theme</button>
 
       <h3>Available Seats ({section})</h3>
-
-      <div
-        style={{
+      
+      <div style={{
           display: "flex",
           justifyContent: "center",
           flexWrap: "wrap",
           gap: "10px",
-          width: "650px",
-        }}
-      >
+          width: "650px"
+        }}>
         {availableSeats.map((seat) => (
           <div
             key={seat}
@@ -76,9 +72,8 @@ const SeatAvailabilityDashboard = () => {
               padding: "10px 16px",
               border: "1px solid black",
               borderRadius: "6px",
-              backgroundColor: "#e0ffe0",
-            }}
-          >
+              backgroundColor: "#e0ffe0"
+            }}>
             Seat {seat}
           </div>
         ))}
@@ -87,4 +82,4 @@ const SeatAvailabilityDashboard = () => {
   );
 };
 
-export default SeatAvailabilityDashboard;
+export default SeatAvailabilityDashboardByUseMemo;
